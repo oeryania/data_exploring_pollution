520805074da9505344e8359a25f9c853eb8587bb
Osama Al-Eryani <oeryania@gmail.com>
2023-10-28 12:53:09 -0400

    put xlabel and ylabel in data definitions, since it is the same for all plots

--- pollution_analysis.R
+++ pollution_analysis.R
@@ -1,13 +1,15 @@
-# if running Source, run Source with echo
+# if running Source, use echo
 # source("pollution_analysis.R", echo=TRUE)
 
 library(ggplot2)
 library(plyr)
 library(dplyr)
 
+# data definitions
 url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
 filenames <- c("summarySCC_PM25.rds", "Source_Classification_Code.rds")
 cities <- data.frame(fips = c("24510", "06037"), city = c("Baltimore", "Los Angeles"))
+xlabel = "Year"; ylabel = "PM2.5 Tons"
 
 # download data, as needed
 if (!all(file.exists(filenames))) {
@@ -23,46 +25,46 @@ if (!exists("data_all")){
     data_all$year = as.factor(data_all$year) # factorize year to plot as discrete units
 }
 
-title <- "1. National Emissions"; x = "Year"; y = "PM2.5 Tons"
+title <- "1. National Emissions"
 data_all %>%
     summarize(pm25 = sum(Emissions), .by = year) %>% # sum emissions by year
-    barplot(pm25 ~ year, data = ., xlab = x, ylab = y, main = title) # plot
+    barplot(pm25 ~ year, data = ., xlab = xlabel, ylab = ylabel, main = title) # plot
 
-title <- "2. Baltimore Emissions"; x = "Year"; y = "PM2.5 Tons"
+title <- "2. Baltimore Emissions"
 data_all %>%
     filter(city == "Baltimore") %>% # filter to Baltimore
     summarize(pm25 = sum(Emissions), .by = year) %>% # sum emissions by year
-    barplot(pm25 ~ year, data = ., xlab = x, ylab = y, main = title) # plot
+    barplot(pm25 ~ year, data = ., xlab = xlabel, ylab = ylabel, main = title) # plot
 
-title <- "3. Baltimore Emissions by Type"; x = "Year"; y = "PM2.5 Tons"
+title <- "3. Baltimore Emissions by Type"
 data_all %>%
     filter(city == "Baltimore") %>% # filter to city
     summarize(pm25 = sum(Emissions), .by = c(year, type)) %>% # sum emissions by year and type
     ggplot(data = ., mapping = aes(x = year, y = pm25)) + 
     facet_grid(~ type, scales = "free_y") +
     geom_col() +
-    labs (title = title, x = x, y = y)
+    labs (title = title, x = xlabel, y = ylabel)
 
-title <- "4. Coal Combustion Emissions"; x = "Year"; y = "PM2.5 Tons"
+title <- "4. Coal Combustion Emissions"
 data_all %>%
     filter(grepl("\\bComb\\b.*\\bCoal\\b", EI.Sector, ignore.case = T)) %>% # filter for source
     summarize(pm25 = sum(Emissions), .by = year) %>%
-    barplot(pm25 ~ year, data = ., xlab = x, ylab = y, main = title) # plot
+    barplot(pm25 ~ year, data = ., xlab = xlabel, ylab = ylabel, main = title) # plot
 
-title <- "5. Baltimore Vehicle Emissions"; x = "Year"; y = "PM2.5 Tons"
+title <- "5. Baltimore Vehicle Emissions"
 data_all %>%
     filter(city == "Baltimore") %>% # filter to city
     filter(grepl("\\bVehicles\\b", EI.Sector, ignore.case = T)) %>% # filter for source
     summarize(pm25 = sum(Emissions), .by = year) %>%
-    barplot(pm25 ~ year, data = ., xlab = x, ylab = y, main = title) # plot
+    barplot(pm25 ~ year, data = ., xlab = xlabel, ylab = ylabel, main = title) # plot
 
-title <- "6. Vehicle Emission Comparison"; x = "Year"; y = "PM2.5 Tons"
+title <- "6. Vehicle Emission Comparison"
 data_all %>%
     filter(city %in% c("Baltimore", "Los Angeles") & grepl("\\bVehicles\\b", EI.Sector, ignore.case = T)) %>%
     summarize(pm25 = sum(Emissions), .by = c(city,year)) %>%
     ggplot(data = ., mapping = aes(x = year, y = pm25)) + 
     facet_wrap(~ city, scales = "free_y") +
     geom_col() +
-    labs (title = title, x = x, y = y)
+    labs (title = title, x = xlabel, y = ylabel)
 
 # if you like my work, you can connect with me on http://linkedin.com/in/oeryani/ 😊
