aabc33e70ec87a080aa917ae538159f6b87a4909
Osama Al-Eryani <oeryania@gmail.com>
2023-10-28 13:00:24 -0400

    added comments

--- pollution_analysis.R
+++ pollution_analysis.R
@@ -40,31 +40,28 @@ title <- "3. Baltimore Emissions by Type"
 data_all %>%
     filter(city == "Baltimore") %>% # filter to city
     summarize(pm25 = sum(Emissions), .by = c(year, type)) %>% # sum emissions by year and type
-    ggplot(data = ., mapping = aes(x = year, y = pm25)) + 
-    facet_grid(~ type, scales = "free_y") +
-    geom_col() +
+    ggplot(data = ., mapping = aes(x = year, y = pm25)) + facet_grid(~ type) + geom_col() +
     labs (title = title, x = xlabel, y = ylabel)
 
 title <- "4. Coal Combustion Emissions"
 data_all %>%
     filter(grepl("\\bComb\\b.*\\bCoal\\b", EI.Sector, ignore.case = T)) %>% # filter for source
-    summarize(pm25 = sum(Emissions), .by = year) %>%
+    summarize(pm25 = sum(Emissions), .by = year) %>% # sum emissions by year
     barplot(pm25 ~ year, data = ., xlab = xlabel, ylab = ylabel, main = title) # plot
 
 title <- "5. Baltimore Vehicle Emissions"
 data_all %>%
     filter(city == "Baltimore") %>% # filter to city
     filter(grepl("\\bVehicles\\b", EI.Sector, ignore.case = T)) %>% # filter for source
-    summarize(pm25 = sum(Emissions), .by = year) %>%
+    summarize(pm25 = sum(Emissions), .by = year) %>% # sum emissions by year
     barplot(pm25 ~ year, data = ., xlab = xlabel, ylab = ylabel, main = title) # plot
 
 title <- "6. Vehicle Emission Comparison"
 data_all %>%
-    filter(city %in% c("Baltimore", "Los Angeles") & grepl("\\bVehicles\\b", EI.Sector, ignore.case = T)) %>%
-    summarize(pm25 = sum(Emissions), .by = c(city,year)) %>%
-    ggplot(data = ., mapping = aes(x = year, y = pm25)) + 
-    facet_wrap(~ city, scales = "free_y") +
-    geom_col() +
+    filter(city %in% c("Baltimore", "Los Angeles")) %>% # filter for cities
+    filter(grepl("\\bVehicles\\b", EI.Sector, ignore.case = T)) %>% # filter for source
+    summarize(pm25 = sum(Emissions), .by = c(city,year)) %>% # sum emissions by year and city
+    ggplot(data = ., mapping = aes(x = year, y = pm25)) + facet_wrap(~ city, scales = "free_y") + geom_col() + # plot
     labs (title = title, x = xlabel, y = ylabel)
 
 # if you like my work, you can connect with me on http://linkedin.com/in/oeryani/ 😊
